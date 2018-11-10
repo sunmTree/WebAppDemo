cb63b76c363b309f655788261b0cfb12ad882621
sunmeng <time_sunm@163.com>
2018-11-10 16:57:38 +0800

    [database] add orm class

--- www/app.py
+++ www/app.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'SunTree'
+__author__ = 'Michael Liao'
+
+'''
+async web application.
+'''
 
 import logging; logging.basicConfig(level=logging.INFO)
-import asyncio
+
+import asyncio, os, json, time
+from datetime import datetime
+
 from aiohttp import web
 
-
-async def index(request):
-    return web.Response(body=b'<h1>Hello, Web App</h1>', content_type='text/html')
-
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>')
 
 async def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
     srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-    logging.info('Start server for 127.0.0.1:9000')
+    logging.info('server started at http://127.0.0.1:9000...')
     return srv
-
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
-loop.run_forever()      # 针对并发
-
+loop.run_forever()
